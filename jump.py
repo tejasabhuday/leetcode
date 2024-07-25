297138bc72f8fb256c7cad6c95dd445b380512d2
Tejas <tejasabuday@gmail.com>
2024-07-26 01:55:25 +0530

    second commit

---
+++ jump.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def canJump(self, nums):
+        max_reachable = 0
+        for i in range(len(nums)):
+            if i > max_reachable:
+                return False
+            max_reachable = max(max_reachable, i + nums[i])
+        return True
